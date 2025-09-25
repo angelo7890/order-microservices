2addb970dddb5456b6b7725737aa7ca675a4d33d
angelo7890 <ar293978@gmail.com>
2025-09-25 14:45:36 -0300

    cricao do modulo de processar pedidos

---
+++ processor-orders/src/main/java/com/anjox/processor_orders/processor_orders/ProcessorOrdersApplication.java
@@ -0,0 +1,13 @@
+package com.anjox.processor_orders.processor_orders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessorOrdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessorOrdersApplication.class, args);
+	}
+
+}
---
+++ processor-orders/src/test/java/com/anjox/processor_orders/processor_orders/ProcessorOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anjox.processor_orders.processor_orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
