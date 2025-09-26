645973c3c96f3b0a83b7481f4acaef29f2ee93e4
angelo7890 <ar293978@gmail.com>
2025-09-26 13:35:54 -0300

    foi criado os repository dos modelos de dados

---
+++ processor-orders/src/main/java/com/anjox/processor_orders/processor_orders/repository/OrderedItemRepository.java
@@ -0,0 +1,11 @@
+﻿package com.anjox.processor_orders.processor_orders.repository;
+
+import com.anjox.processor_orders.processor_orders.model.OrderedItemModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface OrderedItemRepository extends JpaRepository<OrderedItemModel, UUID> {
+}
---
+++ processor-orders/src/main/java/com/anjox/processor_orders/processor_orders/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+﻿package com.anjox.processor_orders.processor_orders.repository;
+
+import com.anjox.processor_orders.processor_orders.model.ProductModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductModel, UUID> {
+}
