0fc4b8caa6440fad7e6210f31df030492152c1b7
angelo7890 <ar293978@gmail.com>
2025-09-28 18:11:26 -0300

    foi criado o listener no modulo de processamento para receber os pedidos da fila do rabbitmq

---
+++ processor-orders/src/main/java/com/anjox/processor_orders/processor_orders/listener/OrderListener.java
@@ -0,0 +1,21 @@
+﻿package com.anjox.processor_orders.processor_orders.listener;
+
+import com.anjox.processor_orders.processor_orders.enums.OrderStatus;
+import com.anjox.processor_orders.processor_orders.model.OrderModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderListener {
+    private Logger log = LoggerFactory.getLogger(OrderListener.class);
+
+    @RabbitListener(queues = "${rabbitmq.queue.name}")
+    public void process(OrderModel orderModel) {
+        orderModel.setStatus(OrderStatus.PROCESSED);
+        log.info("pedido processo {}", orderModel.toString());
+    }
+}
